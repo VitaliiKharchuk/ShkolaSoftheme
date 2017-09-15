73b6443d8843860717c7cf9acce8ee5d527d2bfc
Vitalii <vitalii.kharchuk@gmail.com>
2017-09-15 22:47:41 +0300

    firts  task

---
+++ CSharpHW/Lecture#1/HM/HM/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello  World!  My  name  is   Vitalii!");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
