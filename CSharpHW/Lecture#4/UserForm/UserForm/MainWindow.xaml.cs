faf0272610ccccb72903f9a8899b1e586acfbb8a
Vitalii <vitalii.kharchuk@gmail.com>
2017-09-17 20:22:11 +0300

    fourth

---
+++ CSharpHW/Lecture#4/UserForm/UserForm/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+
+namespace UserForm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            if ( CheckName(_firstName.Text) && CheckName(_lastName.Text) && CheckDate(_birthDate.Text)  && 
+                CheckEmail(_email.Text)  && CheckInfo(_info.Text) )
+         
+            {
+
+                _validationStatus.Text = "Accepted";
+               
+            }
+
+
+            else
+            {
+                _validationStatus.Text = "Not  valid";
+            }
+
+
+                
+
+
+        }
+
+
+
+        private static bool CheckInfo(String _infoString)
+        {
+
+
+            return (_infoString.Length < 2000);
+        }
+
+
+        private static bool CheckPhoneNumber(String _numberString)
+        {
+
+            int len = _numberString.Length;
+
+
+            if (len != 12) return false;
+
+
+            for (int i = 0; i < len; i++)
+            {
+                if ('0' <= _numberString[i] && _numberString[i] <= '9' ) continue;
+
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+
+        private static bool CheckName(String _nameString)
+        {
+
+            int len = _nameString.Length;
+
+
+            if (len >= 255) return false;
+
+
+            for  (int  i = 0; i < len; i++)
+            {
+                if ('a' <= _nameString[i] && _nameString[i] <= 'z' || 'A' <= _nameString[i] && _nameString[i] <= 'Z') continue;
+
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private static bool CheckEmail(String _emailString)
+        {
+
+            int len = _emailString.Length;
+            if (len >= 255) return false;
+
+
+            for (int i = 0; i < len; i++)
+            {
+                if (_emailString[i] == '@') return  true;
+                
+            }
+
+            return false;
+        }
+
+
+
+        private static    bool CheckDate(String dateString)
+        {
+
+            
+
+
+            string[] formats = {"dd/MM/yyyy", "d/MM/yyyy" , "d/M/yyyy", "dd/M/yyyy" };
+
+            DateTime dateValue;
+
+            DateTime.TryParseExact(dateString, formats,
+                                       new CultureInfo("uk-UA"),   //Ukraine  Culture  Info
+                                       DateTimeStyles.None,
+                                       out dateValue);
+            
+
+            
+
+            return (1990 < dateValue.Year && dateValue.Year < DateTime.Now.Year);
+        }
+
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CSharpHW/Lecture#5/GeometryFiagures/GeometryFiagures/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryFiagures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+
+        }
+
+
+
+
+        public  static   void   Run()
+        {
+            
+            while (true)
+            {
+
+
+                label1:
+                Console.WriteLine("Make a   choice");
+                Console.WriteLine("Triangle -  1");
+                Console.WriteLine("Square -  2");
+                Console.WriteLine("Romb -  3");
+                Console.WriteLine("Exit -  4");
+
+
+                
+                int _choice = default(Int32);
+
+                if (!Int32.TryParse(Console.ReadLine(), out _choice))
+                {
+                    Console.WriteLine("Incorrect   choice  value!");
+                    goto label1;
+                }
+
+               else  if  (_choice > 4)
+                {
+                    Console.WriteLine("Incorrect   choice  value!");
+                    goto label1;
+                }
+
+                
+
+                if (_choice == 4)   //  Exit  from  cycle
+                {      
+                        break;  
+                }
+
+
+
+                label2:
+                Console.Write("Figuare's  size = ");
+                
+                int size = default(Int32);
+
+                if ( !Int32.TryParse(Console.ReadLine(),out size))
+                {
+                    Console.WriteLine("Incorrect   size  value!");
+                    goto label2;
+                }
+
+
+                if (_choice == 1)
+                {
+                    DrawTriangle(size);
+                }
+                else if (_choice == 2)
+                {
+                    DrawSquare(size);
+                }
+                else if (_choice == 3)
+                {
+                    DrawRomb(size);
+                }
+
+                else
+                {
+                    goto label1;
+                }
+
+                
+            }
+        }
+
+
+        static  void  DrawTriangle(int size)
+        {
+
+            for (int  i = 0; i <   size; i++ )
+            {
+                
+                for  (int  j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void DrawSquare(int size)
+        {
+
+            for (int i = 0; i < size; i++)
+            {
+
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+
+
+                Console.WriteLine();
+            }
+
+        }
+
+
+        static void DrawRomb(int size)
+        {
+
+            for (int i = 0; i < size; i++)
+            {
+
+                for  (int j = 0; j <= 2*size; j++)
+                {
+
+
+                    if (size - i <= j && j <= size + i)
+                        Console.Write("* ");
+
+                    else Console.Write("  ");
+
+                }
+
+                
+                Console.WriteLine();
+            }
+
+          
+
+
+            for (int i = size ; i >= 0 ; i--)
+            {
+
+                for (int j = 0; j <=  2*size; j++)
+                {
+
+
+                    if (size - i <= j && j <= size + i)
+                        Console.Write("* ");
+
+                    else Console.Write("  ");
+
+                }
+
+
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+
+
+    }
+}
