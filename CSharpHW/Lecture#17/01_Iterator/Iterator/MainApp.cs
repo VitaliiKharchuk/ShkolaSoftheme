d8bc13ca5dada7e1f091f7eb6b6967dd50ff5da1
Vitalii <vitalii.kharchuk@gmail.com>
2017-09-25 04:33:47 +0300

    GenericIterator

---
+++ CSharpHW/Lecture#17/01_Iterator/Iterator/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Iterator
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            ConcreteAggregate<string> a = new ConcreteAggregate<string>();
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
+
+            
+            Console.WriteLine("Iterating over collection of  strings:");
+
+            var iterator1 = new ConcreteIterator<string>(a);
+
+            var item = iterator1.First();
+            while (item != null)
+            {
+                Console.WriteLine(item);
+                item = iterator1.Next();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
